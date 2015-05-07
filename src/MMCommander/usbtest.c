7cb9e2a8510bfce53c1c06cf1e2fc02f2034008a
Eric Cooper <ecc@cmu.edu>
2015-05-06 23:40:21 -0400

    add serial port output to usbtest

--- src/MMCommander/usbtest.c
+++ src/MMCommander/usbtest.c
@@ -1,14 +1,8 @@
+// Print "hello, world" over USB serial port
 
-/***********************************************************************************
-  Filename:     usbtest.c
-
-  Description:  test USB serial port
-
-***********************************************************************************/
-
-/***********************************************************************************
-* INCLUDE
-*/
+#include <stdio.h>
+#include "delay.h"
+#include "serial.h"
 #include "init.h"
 #include "constants.h"
 #include "interrupts.h"
@@ -19,21 +13,14 @@
 #include "usb_uart.h"
 #include "configuration.h"
 #include "util_buffer.h"
-#include <stdio.h>
 
-/******************************************************************************
-* ISR declarations
-* SDCC requires these to be in the file that defines main()
-*/
+// SDCC requires interrrupt handlers to be declared
+// in the file that defines main()
 
 extern void PORT1_ISR(void) __interrupt (P1INT_VECTOR);
 extern void usbirqResumeHandler(void) __interrupt (P0INT_VECTOR);
 extern void usbirqHandler(void) __interrupt (P2INT_VECTOR);
 extern void TIMER1_ISR(void) __interrupt (T1_VECTOR);
-
-/******************************************************************************
-* GLOBAL VARIABLES
-*/
 
 uint8  __xdata uartRxBuffer[SIZE_OF_UART_RX_BUFFER];
 uint8  __xdata uartTxBuffer[SIZE_OF_UART_TX_BUFFER];
@@ -41,28 +28,33 @@ int    __xdata uartTxLength;
 int    __xdata uartTxIndex;
 int    __xdata uartRxIndex;
 
-/******************************************************************************
-* MAIN FUNCTION
-*/
+static int usb_ready = 0;
 
 int main(void)
 {
+    int counter = 0;
+
     /* Configure system */
     initGlobals();
     configureIO();
     configureOsc();
+    serial_init();
+    printf("initializing USB ... ");
     halUartInit(HAL_UART_BAUDRATE_57600, 0);
+    printf("done.\r\n");
+    usb_ready = 1;
 
     while (1) {
-        printf("Hello, world from CC1111!\r\n");
+        printf("Hello, world from CC1111! (%u)\r\n", ++counter);
         usbUartProcess();
-        halMcuWaitMs(1000);
+        delay(1000);
     }
 }
 
 extern ringBuf_t __xdata rbTxBuf;
 
 void putchar(char c) {
-    if (bufNumBytes(&rbTxBuf) != 0)
+    serial_putc(c);
+    if (usb_ready && bufNumBytes(&rbTxBuf) != 0)
         halUartWrite(&c, 1);
 }
